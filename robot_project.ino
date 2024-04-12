e9ed68732f57ee56b903a7e0c8480f1392ca4b71
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2024-04-12 18:39:40 -0400

    moooove

--- robot_project.ino
+++ robot_project.ino
@@ -1,3 +1,50 @@
+
+void forward(){
+
+  DDRD = 0b01010000;    // pin 6 (left) forward and 4 forward(right)
+  PORTD = 0b01010000; // set to high to move forward (use PWM to control speed)
+  _delay_ms(1000); // note will adjust how long delay to account for distance
+  DDRD = 0b00000000;
+} 
+
+void backward(){
+  DDRD = 0b10001000; // pin 7 (left) backward pin 3 backward (right)
+  PORTD = 0b10001000;
+  _delay_ms(1000);
+  DDRD = 0b00000000;
+}
+
+void turnRight(){
+
+  // pin6 forward and pin 3 backward
+  DDRD = 0b01001000;
+  PORTD = 0b01001000;
+  _delay_ms(1000);
+  DDRD = 0b00000000;
+}
+
+void turnLeft(){
+
+    // pin 4 forward and pin 7 backward
+  DDRD = 0b10010000;
+  PORTD = 0b10010000;
+  _delay_ms(1000);
+  DDRD = 0b00000000;
+
+}
+
+
+
 int main(void){
-  return 0;
+  
+  forward()
+  turnRight()
+  forward()
+
+  turnLeft()
+  forward()
+
+  backward()
+
+
 }
