f15f994017756fc8dca03b0c4003370960a641a0
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2024-04-21 16:14:24 -0400

    color and edge detection

--- robot_project.ino
+++ robot_project.ino
@@ -15,6 +15,8 @@ int MIN_BLUE_PERIOD = 325;
 int MAX_YELLOW_PERIOD = 128;
 int MIN_YELLOW_PERIOD = 35;
 
+char homeColor[] = "";
+
 void forward()
 {
   DDRD = 0b01010000;  // pin 6 (left) forward and 4 forward(right)
@@ -41,7 +43,6 @@ void turnLeft()
   PORTD = 0b10010000;
 }
 
-
 // Interrupt service routine (color sensor)
 ISR(PCINT0_vect)
 {
@@ -66,7 +67,7 @@ void initColor()
   // Initialize timer
   TCCR1A = 0b00000000; // Normal Mode
   TCCR1B = 0b00000001; // 1 pre-scalar
-  TCINT1 = 0;          // reset timer
+  TCNT1 = 0;           // reset timer
 }
 
 int getColor()
@@ -90,11 +91,23 @@ int main(void)
   DDRB = 0b00000000; // ALL pins on B as inputs.
   Serial.begin(9600);
   initColor();
-  _delay_ms(10)
+  _delay_ms(10);
   while (1)
   {
     getColor();
     Serial.println(colorSensorPeriod);
+
+    // if (MIN_BLUE_PERIOD <= colorSensorPeriod && colorSensorPeriod <= MAX_BLUE_PERIOD)
+    // {
+    //   homeColor = "blue";
+    // }
+    // else if (MIN_YELLOW_PERIOD <= colorSensorPeriod && colorSensorPeriod <= MAX_YELLOW_PERIOD)
+    // {
+    //   homeColor = "yellow";
+    // }
+
+    // less than 150 = yellow
+    // greater than 200 = blue
 
     bool edge_left = PINB & PIN_QTI_LEFT;
     bool edge_right = PINB & PIN_QTI_RIGHT;
@@ -103,7 +116,6 @@ int main(void)
     Serial.print(", edge_right:");
     Serial.print(edge_right);
     Serial.println();
-
 
     // Color sensor detection logic (Milestone 3):
     // 1. start by noting home color
@@ -117,7 +129,6 @@ int main(void)
     // if edge_left is true and edge_right is false, then turn right (90 degrees)
     // if edge_left is true and edge_right is false, then turn left (90 degrees)
 
-
-    _delay_ms(1000);
+    // _delay_ms(10);
   }
 }
