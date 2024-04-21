14e17153b385846d8aa4f68e2d54cdc070058665
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2024-04-21 14:35:50 -0400

    include only color-sensor code

--- robot_project.ino
+++ robot_project.ino
@@ -1,21 +1,25 @@
-void forward(){
-  DDRD = 0b01010000;    // pin 6 (left) forward and 4 forward(right)
+void forward()
+{
+  DDRD = 0b01010000;  // pin 6 (left) forward and 4 forward(right)
   PORTD = 0b01010000; // set to high to move forward (use PWM to control speed)
-} 
+}
 
-void backward(){
+void backward()
+{
   DDRD = 0b10001000; // pin 7 (left) backward pin 3 backward (right)
   PORTD = 0b10001000;
 }
 
-void turnRight(){
+void turnRight()
+{
   // pin6 forward and pin 3 backward
   DDRD = 0b01001000;
   PORTD = 0b01001000;
 }
 
-void turnLeft(){
-    // pin 4 forward and pin 7 backward
+void turnLeft()
+{
+  // pin 4 forward and pin 7 backward
   DDRD = 0b10010000;
   PORTD = 0b10010000;
 }
@@ -41,8 +45,9 @@ ISR(PCINT0_vect)
   }
 }
 
-void initColor(){
-  // DDRB = 0b00000000; // ALL pins on B as inputs. 
+void initColor()
+{
+  // DDRB = 0b00000000; // ALL pins on B as inputs.
   sei();
 
   // Initialize interrupts
@@ -51,12 +56,11 @@ void initColor(){
   // Initialize timer
   TCCR1A = 0b00000000; // Normal Mode
   TCCR1B = 0b00000001; // 1 pre-scalar
-  TCINT1 = 0; //reset timer
- 
+  TCINT1 = 0;          // reset timer
 }
 
-int getColor(){
-
+int getColor()
+{
 
   PCMSK0 |= 0b00000100; // Enable pin change interrupt for pin 10
 
@@ -66,56 +70,19 @@ int getColor(){
 
   PCMSK0 &= ~0b00000100; // Disable pin change interrupt for pin 10
 
-  period = timer1Value * 0.0625 * 2;
-
+  period = timer1Value * 0.0625 * 2; // in microseconds
 }
 
+int main(void)
+{
 
-int main(void){
-
-
- // Initialize color sensor
- 
-//  Serial.begin(9600);
-//  initColor();
-//  sei();
-
- // while(1){
-    //getColor();
-
-    //Serial.println(period);
-       
-    forward();
-    _delay_ms(2000);
-
-    turnRight();
-    _delay_ms(700);
-
-    forward();
-    _delay_ms(2000);
-
-    turnLeft();
-    _delay_ms(700);
-
-    forward();
+  // Initialize color sensor
+  Serial.begin(9600);
+  initColor();
+  while (1)
+  {
+    getColor();
+    Serial.println(period);
     _delay_ms(1000);
-
-    backward();
-    _delay_ms(3000);
-
-    turnLeft();
-    _delay_ms(700);
-
-    forward();
-    _delay_ms(2000);
-
-    turnRight();
-    _delay_ms(700);
-
-    DDRD = 0b00000000; 
-
-    _delay_ms(1000);
-  
-
+  }
 }
-  
